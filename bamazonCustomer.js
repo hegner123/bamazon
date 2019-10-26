5b5897a93472e98c6e24c161c1a2249ed477cf70
Michael Hegner <hegner123@gmail.com>
2019-10-26 12:18:46 -0400

    added first db query. passed local testing

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,55 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "password"
+  password: "password",
+  database: "bamazon"
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
+  inquirer
+    .prompt([
+      {
+      name: 'menu',
+      message: 'Welcome To bamazon',
+      type: 'list',
+      choices: ['yes','no']
+    }
+  ])
+    .then(answers => {
+     if (answers.menu === 'yes'){
+       testConnect();
+     } else {
+       console.log('goodbye');
+     }
+    });
+
+    function testConnect(){
+      con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM products", function (err, result, fields) {
+          if (err) throw err;
+          console.clear();
+          console.log(result);
+          buy();
+        });
+      });
+    };
+    function buy(){
+      inquirer
+      .prompt([{
+        name: 'buy',
+        message: 'What Item do you want to buy?',
+        type: 'number'
+      }]).then(answers =>{
+       dbSearch(answers.buy);
+      });
+    };
+    function dbSearch(parm){
+    con.query("SELECT * FROM products WHERE item_id=" + parm, function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+          con.end();
+        });
+    };
