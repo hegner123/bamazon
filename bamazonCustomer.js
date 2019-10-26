bc96ce6749e1d03873f427f010aa8ce65ef51b2b
Michael Hegner <hegner123@gmail.com>
2019-10-26 12:33:55 -0400

    formatted response

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,48 +8,53 @@ var con = mysql.createConnection({
   database: "bamazon"
 });
 
-  inquirer
-    .prompt([
-      {
-      name: 'menu',
-      message: 'Welcome To bamazon',
-      type: 'list',
-      choices: ['yes','no']
+console.clear();
+inquirer
+  .prompt([
+    {
+    name: 'menu',
+    message: 'Welcome To bamazon, would you like to shop?',
+    type: 'list',
+    choices: ['yes','no']
+  }
+])
+  .then(answers => {
+    if (answers.menu === 'yes'){
+      testConnect();
+    } else {
+      console.log('goodbye');
     }
-  ])
-    .then(answers => {
-     if (answers.menu === 'yes'){
-       testConnect();
-     } else {
-       console.log('goodbye');
-     }
-    });
+  });
 
-    function testConnect(){
-      con.connect(function(err) {
+  function testConnect(){
+    con.connect(function(err) {
+      if (err) throw err;
+      con.query("SELECT * FROM products", function (err, result, fields) {
         if (err) throw err;
-        con.query("SELECT * FROM products", function (err, result, fields) {
-          if (err) throw err;
-          console.clear();
-          console.log(result);
-          buy();
-        });
+        console.clear();
+        console.log(result[0].product_name);
+        buy();
       });
-    };
-    function buy(){
-      inquirer
-      .prompt([{
-        name: 'buy',
-        message: 'What Item do you want to buy?',
-        type: 'number'
-      }]).then(answers =>{
-       dbSearch(answers.buy);
-      });
-    };
-    function dbSearch(parm){
+    });
+  };
+
+  function buy(){
+    inquirer
+    .prompt([{
+      name: 'buy',
+      message: 'What Item do you want to buy?',
+      type: 'number'
+    }]).then(answers =>{
+      dbSearch(answers.buy);
+    });
+  };
+
+  function dbSearch(parm){
     con.query("SELECT * FROM products WHERE item_id=" + parm, function (err, result, fields) {
-          if (err) throw err;
-          console.log(result);
-          con.end();
-        });
-    };
+      if (err) throw err;
+      console.log('----------------------------------------------');
+      console.log(result[0].product_name  + ' | ' + result[0].department_name +  " | $" + result[0].price + ' | ' + result[0].stock_quantity);
+      console.log('----------------------------------------------');
+      con.end();
+    });
+  };
